6361032a02257a70c0fb4b094cf078b1c3d89334
Theo <siwrist@yahoo.gr>
2019-07-24 17:30:35 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as Converter from 'units-converter';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'units-converter-consumer';
+export class AppComponent implements OnInit {
+  private functions: string[] = [];
+  private units: any[] = [];
+  private fromUnit: string;
+  private toUnit: string;
+  private selected: string;
+  private fromValue: number;
+  private toValue: number;
+
+  // Where True is for fromValue and False for toValue
+  private lastChanged = true;
+
+  private onSelectionChangeType() {
+    this.units = Converter[this.selected]().list();
+    this.fromUnit = this.units[0].unit;
+    this.toUnit = this.units[1].unit;
+    this.fromValue = 1;
+    this.toValue = Converter[this.selected](this.fromValue).from(this.fromUnit).to(this.toUnit).value;
+  }
+
+  private onSelectionChangeFrom(newValue?) {
+
+    if (newValue && newValue.target.value as number) {
+
+      this.toValue = (newValue.target.value as number > 1) ? newValue.target.value.replace(/^0+/, '') as number : newValue.target.value as number;
+    } else if (newValue && newValue.target.value === '') {
+      this.toValue = 0;
+    }
+
+    this.fromValue = Converter[this.selected](this.toValue).from(this.fromUnit).to(this.toUnit).value;
+  }
+
+
+  private onSelectionChangeTo(newValue?) {
+
+    if (newValue && newValue.target.value as number) {
+      this.fromValue = (newValue.target.value as number > 1) ? newValue.target.value.replace(/^0+/, '') as number : newValue.target.value as number;
+    } else if (newValue && newValue.target.value === '') {
+      this.fromValue = 0;
+    }
+
+    this.toValue = Converter[this.selected](this.fromValue).from(this.fromUnit).to(this.toUnit).value;
+  }
+
+  ngOnInit(): void {
+    this.functions = Object.keys(Converter);
+
+    console.log(Converter[this.functions[1]]().list());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
